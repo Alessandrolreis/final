4a5ea924310652ccc2340a248ca6b682b38edcb7
alessandro <alessandro@DESKTOP-4PJ9450>
2021-05-27 03:27:15 -0300

    Adicionar arquivos de projeto.

---
+++ final/Controllers/TodoItemsController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace final.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TodoItemsController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+        public TodoItemsController(TodoContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/TodoItems
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetTodoItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+        // GET: api/TodoItems/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            return todoItem;
+        }
+
+        // GET: api/TodoItems/Verifica/email
+        [HttpGet("Verifica/{email}")]
+
+        
+        public ActionResult<TodoItem> GetTodoItem(string email)
+        {
+
+            var todoItem =  _context.TodoItems.FirstOrDefault(a => a.email.Contains(email));
+                                
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+            
+             return todoItem;
+        }
+
+        // PUT: api/TodoItems/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutTodoItem(long id, TodoItem todoItem)
+        {
+            if (id != todoItem.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(todoItem).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!TodoItemExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/TodoItems
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for
+        // more details, see https://go.microsoft.com/fwlink/?linkid=2123754.
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> PostTodoItem(TodoItem todoItem)
+        {
+                string numerocardfinal;
+                string numerocard0;
+                string numerocard1;
+                string numerocard2;
+                string numerocard3;
+                Random a = new Random();
+                numerocard0 = a.Next(1000, 9000).ToString();
+                numerocard1 = a.Next(1000, 9000).ToString();
+                numerocard2 = a.Next(1000, 9000).ToString();
+                numerocard3 = a.Next(1000, 9000).ToString();
+                numerocardfinal = string.Concat(numerocard0, numerocard1, numerocard2, numerocard3);
+                todoItem.card = numerocardfinal;
+                _context.TodoItems.Add(todoItem);        
+                await _context.SaveChangesAsync();            
+
+            //return CreatedAtAction("GetTodoItem", new { id = todoItem.Id }, todoItem);
+            return CreatedAtAction(nameof(GetTodoItem), new { id = todoItem.Id },  todoItem);
+            
+        }
+
+        // DELETE: api/TodoItems/5
+        [HttpDelete("{id:long}")]
+        public async Task<ActionResult<TodoItem>> DeleteTodoItem(long id)
+        {
+            var todoItem = await _context.TodoItems.FindAsync(id);
+            if (todoItem == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todoItem);
+            await _context.SaveChangesAsync();
+
+            return todoItem;
+        }
+
+        private bool TodoItemExists(long id)
+        {
+            return _context.TodoItems.Any(e => e.Id == id);
+        }
+    }
+}
---
+++ final/Models/TodoItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+namespace TodoApi.Models
+{
+    public class TodoItem
+    {
+        public long Id { get; set; }
+        public string email { get; set; }
+        public string card { get; set; }
+
+        
+    }
+}
